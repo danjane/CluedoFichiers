45f1d71675109c6256b9f03e00a178dd982c3e8a
danjane <jdtjane@gmail.com>
2021-05-30 23:42:38 +0200

    Get ready to build and tear down folder structures

--- src/CluedoFichiers/dossiers.py
+++ src/CluedoFichiers/dossiers.py
@@ -1,8 +1,35 @@
-from CluedoFichiers.casebook import CaseBook
-from CluedoFichiers.alibis import Alibis
+import os
 
 
 class Dossiers:
 
-    def __init__(self):
+    def __init__(self, path, pistes):
+        self.pistes = pistes
+        if path:
+            self.path = path
+        else:
+            self.path = os.path.join(
+                os.getcwd(), "Cobaye")
+        if os.path.basename(self.path) == "Cobaye":
+            self.nettoyage()
+        else:
+            raise RuntimeError
+        os.mkdir(self.path)
+
+    def plat(self):
         pass
+
+    def nettoyage(self):
+        if os.path.isfile(self.path):
+            raise RuntimeError
+        if os.path.isdir(self.path) and (os.path.basename(self.path) == "Cobaye"):
+            # Delete everything reachable from the directory named in 'self.path',
+            # assuming there are no symbolic links.
+            # CAUTION:  This is dangerous!  For example, if top == '/', it
+            # could delete all your disk files.
+            for root, dirs, files in os.walk(self.path, topdown=False):
+                for name in files:
+                    os.remove(os.path.join(root, name))
+                for name in dirs:
+                    os.rmdir(os.path.join(root, name))
+            os.rmdir(self.path)
--- tests/test_dossiers.py
+++ tests/test_dossiers.py
@@ -2,4 +2,9 @@ from CluedoFichiers.dossiers import Dossiers
 
 
 def test_construction():
-    assert Dossiers()
+    assert Dossiers(None, [])
+
+
+def test_plat():
+    dossiers = Dossiers(None, ["test1", "test2"])
+    dossiers.plat()
