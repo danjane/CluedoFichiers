ced9c930c4b2729c868f8899f99c45e72a3a501c
danjane <jdtjane@gmail.com>
2021-05-30 17:08:55 +0200

    Build the list of alibis

--- src/CluedoFichiers/alibis.py
+++ src/CluedoFichiers/alibis.py
@@ -2,3 +2,12 @@ class Alibis:
 
     def __init__(self, casebook):
         self.__casebook = casebook
+        self.__size = self.__casebook.size
+        self.elements = self.build_elements()
+
+    def build_elements(self):
+        L = []
+        for i in range(1, self.__size):
+            L.append(list(self.__casebook[i].values()))
+        return L
+
--- tests/test_alibis.py
+++ tests/test_alibis.py
@@ -6,6 +6,16 @@ def test_alibis():
     assert True
 
 
-def test_consruction():
+def test_construction():
     casebook = CaseBook(1)
     assert Alibis(casebook)
+
+
+def test_first_clues():
+    casebook = CaseBook(2)
+    alibis = Alibis(casebook)
+    assert alibis.elements[0] == [
+        "Colonel Michael Moutarde",
+        "la salle de bal",
+        "chandelier"
+    ]
