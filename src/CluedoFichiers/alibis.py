5c7300697185710553675b82bf0d0e6f727f3802
danjane <jdtjane@gmail.com>
2021-05-30 17:26:46 +0200

    Ability to select a new set of unique clues

--- src/CluedoFichiers/alibis.py
+++ src/CluedoFichiers/alibis.py
@@ -1,14 +1,25 @@
+import itertools
+
+
 class Alibis:
 
     def __init__(self, casebook):
         self.__casebook = casebook
-        self.__size = self.__casebook.size
+        self.__size = self.__casebook.size-1
         self.elements = self.build_elements()
-        self.clue_index = [0]
+        self.__indices = list(
+            itertools.product(range(4), repeat=self.__size)
+            )
+        self.clue_index = self.__indices[0]
 
     def build_elements(self):
         L = []
-        for i in range(1, self.__size):
+        for i in range(self.__size):
             L.append(list(self.__casebook[i].values()))
         return L
+
+    def new_clue_index(self, pas):
+        self.clue_index = self.__indices[int(pas) % len(self.__indices)]
+
+
 
--- tests/test_alibis.py
+++ tests/test_alibis.py
@@ -15,13 +15,22 @@ def test_first_set_of_alibis():
     casebook = CaseBook(2)
     alibis = Alibis(casebook)
     assert alibis.elements[0] == [
-        "Colonel Michael Moutarde",
-        "la salle de bal",
-        "chandelier"
+        "Mademoiselle Josephine Rose",
+        "la salle de billard",
+        "poignard"
     ]
 
 
 def test_clue_selection():
     casebook = CaseBook(2)
     alibis = Alibis(casebook)
-    assert alibis.clue_index == [0]
+    assert alibis.clue_index == (0, )
+
+
+def test_new_clue_selection():
+    casebook = CaseBook(3)
+    alibis = Alibis(casebook)
+    assert alibis.clue_index == (0, 0)
+    alibis.new_clue_index(1)
+    assert alibis.clue_index == (0, 1)
+
