d310856babf759a3b86498840a69a015f791e9d8
danjane <jdtjane@gmail.com>
2021-06-02 23:06:17 +0200

    Filenames are automatically numbered if there are repeats

--- src/CluedoFichiers/dossiers.py
+++ src/CluedoFichiers/dossiers.py
@@ -3,9 +3,11 @@ import os
 
 class Dossiers:
 
-    def __init__(self, path, pistes):
-        self.pistes = pistes
-        self.fichier_num = 0
+    def __init__(self, path, pistes, noms=None):
+        if not noms:
+            noms = ["Notes"] * len(pistes)
+        filenames = self.construire_filenames(noms)
+        self.pistes = list(zip(pistes, filenames))
         if path:
             self.path = path
         else:
@@ -36,13 +38,37 @@ class Dossiers:
                     os.rmdir(os.path.join(root, name))
             os.rmdir(self.path)
 
-    def fichier_name(self):
-        name = "Notes_{:02d}.txt".format(self.fichier_num)
-        self.fichier_num += 1
-        return name
+    @staticmethod
+    def fichier(piste, path):
+        with open(os.path.join(path, piste[1]), 'w') as file:
+            file.write(piste[0])
 
-    def fichier(self, piste, path, name=None):
-        if not name:
-            name = self.fichier_name()
-        with open(os.path.join(path, name), 'w') as file:
-            file.write(piste)
+    def liste(self):
+        self.liste_recursive(self.path, self.pistes)
+
+    def liste_recursive(self, path, pistes):
+        self.fichier(pistes[0], path)
+        if len(pistes) > 1:
+            path = os.path.join(path, "NewFolder")
+            os.mkdir(path)
+            self.liste_recursive(path, pistes[1:])
+
+    @staticmethod
+    def construire_filenames(noms):
+        compteur = {}
+        filenames = []
+        for nom in noms:
+            if not nom:
+                nom = "Notes"
+            if nom in compteur:
+                compteur[nom] += 1
+            else:
+                compteur[nom] = 0
+            filenames.append("{}_{:02d}.txt".format(nom, compteur[nom]))
+
+        for (key, value) in compteur.items():
+            if value == 0:
+                filenames = [
+                    x.replace(f"{key}_00.txt", f"{key}.txt") for x in filenames
+                ]
+        return filenames
--- tests/test_dossiers.py
+++ tests/test_dossiers.py
@@ -13,3 +13,30 @@ def test_plat():
         os.path.join(dossiers.path, "Notes_00.txt"))
     assert os.path.isfile(
         os.path.join(dossiers.path, "Notes_01.txt"))
+
+
+def test_liste():
+    dossiers = Dossiers(None, ["test1", "test2"])
+    dossiers.liste()
+    assert os.path.isfile(
+        os.path.join(dossiers.path, "Notes_00.txt"))
+    assert os.path.isfile(
+        os.path.join(dossiers.path, "NewFolder", "Notes_01.txt"))
+
+
+def test_noms_AA():
+    dossiers = Dossiers(None, ["test1", "test2"], ["A", "A"])
+    dossiers.plat()
+    assert os.path.isfile(
+        os.path.join(dossiers.path, "A_00.txt"))
+    assert os.path.isfile(
+        os.path.join(dossiers.path, "A_01.txt"))
+
+
+def test_noms_AB():
+    dossiers = Dossiers(None, ["test1", "test2"], ["A", "B"])
+    dossiers.plat()
+    assert os.path.isfile(
+        os.path.join(dossiers.path, "A.txt"))
+    assert os.path.isfile(
+        os.path.join(dossiers.path, "B.txt"))
